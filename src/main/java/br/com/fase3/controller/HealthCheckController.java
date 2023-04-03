dd512aae0b31f936febda69affcfc4ed3378464f
Leo Borba <guimili@live.com>
2023-04-03 17:42:03 -0300

    adjusted project configs

--- src/main/java/br/com/fase3/controller/HealthCheckController.java
+++ src/main/java/br/com/fase3/controller/HealthCheckController.java
@@ -5,13 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
 
     @GetMapping
-    public ResponseEntity<String> status(){
-        return ResponseEntity.ok("Rodando");
+    public ResponseEntity<String> status() throws IOException{
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+
+        properties.load(inputStream);
+
+        return ResponseEntity.ok("Running on port: " + properties.get("port"));
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+
+        properties.load(inputStream);
+
+        return ResponseEntity.ok(properties.get("name") + "-" + properties.get("version"));
     }
 
 }
